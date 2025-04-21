8868931ea56e1f1fd0cabdb6810a8cfdfdcee3d3
Tommaso Bolis <tbolis@mulesoft.com>
2025-04-21 20:01:20 +0200

    Migrate Mistral AI Connection to org.mule.runtime.http.api.client.HttpClient and add Proxy support

--- src/main/java/org/mule/extension/vectors/internal/model/text/mistralai/MistralAIModel.java
+++ src/main/java/org/mule/extension/vectors/internal/model/text/mistralai/MistralAIModel.java
@@ -1,28 +1,26 @@
 package org.mule.extension.vectors.internal.model.text.mistralai;
 
 import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.model.mistralai.MistralAiEmbeddingModel;
 import org.mule.extension.vectors.internal.config.EmbeddingConfiguration;
 import org.mule.extension.vectors.internal.connection.model.mistralai.MistralAIModelConnection;
 import org.mule.extension.vectors.internal.helper.parameter.EmbeddingModelParameters;
 import org.mule.extension.vectors.internal.model.BaseModel;
 
-public class MistralAIModel  extends BaseModel {
+public class MistralAIModel extends BaseModel {
 
-  private final String apiKey;
+    private final MistralAIModelConnection mistralAIModelConnection;
 
-  public MistralAIModel(EmbeddingConfiguration embeddingConfiguration, MistralAIModelConnection mistralAIModelConnection, EmbeddingModelParameters embeddingModelParameters) {
+    public MistralAIModel(EmbeddingConfiguration embeddingConfiguration, 
+                         MistralAIModelConnection mistralAIModelConnection,
+                         EmbeddingModelParameters embeddingModelParameters) {
+        super(embeddingConfiguration, mistralAIModelConnection, embeddingModelParameters);
+        this.mistralAIModelConnection = mistralAIModelConnection;
+    }
 
-    super(embeddingConfiguration, mistralAIModelConnection, embeddingModelParameters);
-
-    this.apiKey = mistralAIModelConnection.getApiKey();
-  }
-
-  public EmbeddingModel buildEmbeddingModel() {
-
-    return MistralAiEmbeddingModel.builder()
-        .apiKey(apiKey)
-        .modelName(embeddingModelParameters.getEmbeddingModelName())
-        .build();
-  }
+    public EmbeddingModel buildEmbeddingModel() {
+        return MistralAIEmbeddingModel.builder()
+            .connection(mistralAIModelConnection)
+            .modelName(embeddingModelParameters.getEmbeddingModelName())
+            .build();
+    }
 }
