71da46b4dd41392a649b5d2cba7d11e69abb31f6
Tommaso Bolis <tbolis@mulesoft.com>
2025-05-26 17:18:19 +0200

    Remove langchain4j dependencies and replace with native AWS SDK for S3 integration
    - Replaced `langchain4j-document-loader-amazon-s3` dependency with `software.amazon.awssdk:s3`.
    - Refactored `AmazonS3Storage` to eliminate reliance on `AmazonS3DocumentLoader` and `AwsCredentials`.
    - Introduced a `loadDocument` method in `AmazonS3StorageConnection` using AWS SDK's native API for document loading.
    - Adjusted related code to work with the new implementation, ensuring existing functionality remains intact.

--- src/main/java/org/mule/extension/vectors/internal/connection/storage/amazons3/AmazonS3StorageConnection.java
+++ src/main/java/org/mule/extension/vectors/internal/connection/storage/amazons3/AmazonS3StorageConnection.java
@@ -1,11 +1,19 @@
 package org.mule.extension.vectors.internal.connection.storage.amazons3;
 
+import dev.langchain4j.data.document.BlankDocumentException;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentParser;
 import org.mule.extension.vectors.internal.connection.storage.BaseStorageConnection;
 import org.mule.extension.vectors.internal.constant.Constants;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+
+import static dev.langchain4j.internal.ValidationUtils.ensureNotBlank;
+import static java.lang.String.format;
 
 public class AmazonS3StorageConnection implements BaseStorageConnection {
 
@@ -65,5 +73,25 @@ public class AmazonS3StorageConnection implements BaseStorageConnection {
 
     this.s3Client.listBuckets();
     return true;
+  }
+
+  public Document loadDocument(String bucket, String key, DocumentParser parser) {
+    try {
+      GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+          .bucket(ensureNotBlank(bucket, "bucket"))
+          .key(ensureNotBlank(key, "key"))
+          .build();
+      ResponseInputStream<GetObjectResponse> inputStream = s3Client.getObject(getObjectRequest);
+      Document document = parser.parse(inputStream);
+      document.metadata().put(Constants.METADATA_KEY_SOURCE, format("s3://%s/%s", bucket, key));
+      return document;
+
+    } catch (S3Exception e) {
+      throw new RuntimeException(e);
+    } catch (BlankDocumentException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to load document", e);
+    }
   }
 }
