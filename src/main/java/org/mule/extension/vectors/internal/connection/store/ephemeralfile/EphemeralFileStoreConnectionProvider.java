9eb16d191bed39a74a76dac4df012a550ba8ac92
Tommaso Bolis <tbolis@mulesoft.com>
2025-05-27 13:36:36 +0200

    Add LangChain4J external library annotation to EphemeralFileStoreConnectionProvider
    - Update version in `pom.xml` to `0.5.44-SNAPSHOT`.
    - Add `@ExternalLib` annotation to specify LangChain4J as a dependency in `EphemeralFileStoreConnectionProvider`.
    - Define required class, name pattern, and library coordinates for LangChain4J.

--- src/main/java/org/mule/extension/vectors/internal/connection/store/ephemeralfile/EphemeralFileStoreConnectionProvider.java
+++ src/main/java/org/mule/extension/vectors/internal/connection/store/ephemeralfile/EphemeralFileStoreConnectionProvider.java
@@ -5,14 +5,23 @@ import org.mule.extension.vectors.internal.connection.store.BaseStoreConnectionP
 import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.runtime.api.connection.ConnectionValidationResult;
 import org.mule.runtime.extension.api.annotation.Alias;
+import org.mule.runtime.extension.api.annotation.ExternalLib;
 import org.mule.runtime.extension.api.annotation.param.ParameterGroup;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 import org.mule.runtime.extension.api.annotation.param.display.Placement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.mule.runtime.api.meta.ExternalLibraryType.DEPENDENCY;
+
 @Alias("ephemeralFile")
 @DisplayName("Ephemeral File")
+@ExternalLib(name = "LangChain4J",
+    type=DEPENDENCY,
+    description = "LangChain4J",
+    nameRegexpMatcher = "(.*)\\.jar",
+    requiredClassName = "dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore",
+    coordinates = "dev.langchain4j:langchain4j:1.0.1")
 public class EphemeralFileStoreConnectionProvider  extends BaseStoreConnectionProvider {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(EphemeralFileStoreConnectionProvider.class);
