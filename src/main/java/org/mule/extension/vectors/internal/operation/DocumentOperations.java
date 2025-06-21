d3ccc57eddf8a82b547e980f36dc89353abb0a24
Tommaso Bolis <tbolis@mulesoft.com>
2025-06-21 23:52:57 +0200

    Refactor document operations to rename response schemas and update chunking logic for improved clarity and consistency

--- src/main/java/org/mule/extension/vectors/internal/operation/DocumentOperations.java
+++ src/main/java/org/mule/extension/vectors/internal/operation/DocumentOperations.java
@@ -2,14 +2,10 @@ package org.mule.extension.vectors.internal.operation;
 
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.DocumentParser;
-import dev.langchain4j.data.document.Metadata;
 import dev.langchain4j.data.segment.TextSegment;
-import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.mule.extension.vectors.internal.config.DocumentConfiguration;
-import org.mule.extension.vectors.internal.util.MetadataUtils;
 
-import org.json.JSONObject;
 import org.mule.extension.vectors.api.metadata.DocumentResponseAttributes;
 import org.mule.extension.vectors.internal.constant.Constants;
 import org.mule.extension.vectors.internal.error.MuleVectorsErrorType;
@@ -17,11 +13,9 @@ import org.mule.extension.vectors.internal.error.provider.DocumentErrorTypeProvi
 import org.mule.extension.vectors.internal.helper.parameter.DocumentPayloadParameters;
 import org.mule.extension.vectors.internal.helper.parameter.SegmentationParameters;
 import org.mule.extension.vectors.internal.storage.BaseStorage;
-import org.mule.extension.vectors.internal.util.JsonUtils;
 import org.mule.extension.vectors.internal.util.Utils;
 import org.mule.runtime.extension.api.annotation.Alias;
 import org.mule.runtime.extension.api.annotation.error.Throws;
-import org.mule.runtime.extension.api.annotation.metadata.fixed.InputJsonType;
 import org.mule.runtime.extension.api.annotation.metadata.fixed.OutputJsonType;
 import org.mule.runtime.extension.api.annotation.param.*;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
@@ -32,9 +26,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.stream.IntStream;
 
 import static org.mule.extension.vectors.internal.helper.ResponseHelper.createDocumentResponse;
 import static org.mule.runtime.extension.api.annotation.param.MediaType.APPLICATION_JSON;
@@ -55,7 +47,7 @@ public class DocumentOperations {
   @Alias("Document-parse")
   @DisplayName("[Document] Parse")
   @Throws(DocumentErrorTypeProvider.class)
-  @OutputJsonType(schema = "api/metadata/DocumentResponse.json")
+  @OutputJsonType(schema = "api/metadata/DocumentParseResponse.json")
   public org.mule.runtime.extension.api.runtime.operation.Result<InputStream, DocumentResponseAttributes>
   parse(@Config DocumentConfiguration documentConfiguration,
         @ParameterGroup(name = "Payload") DocumentPayloadParameters payloadParameters) {
@@ -84,10 +76,8 @@ public class DocumentOperations {
       DocumentParser documentParser = BaseStorage.getDocumentParser(payloadParameters.getFileParserType());
       Document document = documentParser.parse(documentStream);
 
-      JSONObject jsonObject = JsonUtils.docToTextSegmentsJson(document);
-
       return createDocumentResponse(
-          jsonObject.toString(),
+          document.text(),
           new HashMap<String, Object>() {{
             put("payloadContentFormat", payloadParameters.getFormat());
           }});
@@ -98,65 +88,6 @@ public class DocumentOperations {
     } catch (Exception e) {
       throw new ModuleException(
           String.format("Error while parsing document."),
-          MuleVectorsErrorType.DOCUMENT_OPERATIONS_FAILURE,
-          e);
-    }
-  }
-
-  /**
-   * Chunk a text segment from a parsed document into multiple text segments based on the provided  parameters.
-   * <p>
-   * This operation expects an input JSON containing an array of one text segment, with associated metadata.
-   * It chunks each text segment into smaller text segments based on the parameters provided, and returns
-   * the result as a JSON document with the chunked text segments and metadata.
-   * </p>
-   *
-   * @param content InputStream containing the JSON document with text segments and metadata.
-   * @param segmentationParameters Parameters that define how the document should be segmented.
-   * @return Result containing the chunked text segments and response attributes.
-   */
-  @MediaType(value = APPLICATION_JSON, strict = false)
-  @Alias("Document-chunk")
-  @DisplayName("[Document] Chunk")
-  @Throws(DocumentErrorTypeProvider.class)
-  @OutputJsonType(schema = "api/metadata/DocumentResponse.json")
-  public org.mule.runtime.extension.api.runtime.operation.Result<InputStream, DocumentResponseAttributes>
-  chunkDocument(@Config DocumentConfiguration documentConfiguration,
-        @Alias("textSegment") @DisplayName("Text Segment") @InputJsonType(schema = "api/metadata/DocumentResponse.json") @Content InputStream content,
-        @ParameterGroup(name = "Segmentation") SegmentationParameters segmentationParameters) {
-
-    try {
-
-      String contentString = IOUtils.toString(content, StandardCharsets.UTF_8);
-
-      List<TextSegment> textSegments = new LinkedList<>();
-
-      JSONObject jsonObject = new JSONObject(contentString);
-      JSONArray jsonTextSegments = jsonObject.getJSONArray(Constants.JSON_KEY_TEXT_SEGMENTS);
-
-      IntStream.range(0, jsonTextSegments.length())
-          .mapToObj(jsonTextSegments::getJSONObject) // Convert index to JSONObject
-          .forEach(jsonTextSegment -> {
-            Metadata metadata = Metadata.from(jsonTextSegment.getJSONObject(Constants.JSON_KEY_METADATA).toMap());
-            textSegments.add(new TextSegment(jsonTextSegment.getString(Constants.JSON_KEY_TEXT), metadata));
-          });
-
-      Document document = Document.from(textSegments.get(0).text(), textSegments.get(0).metadata());
-
-      JSONObject responseJsonObject = JsonUtils.docToTextSegmentsJson(document,
-                                                              segmentationParameters.getMaxSegmentSizeInChars(),
-                                                              segmentationParameters.getMaxOverlapSizeInChars());
-
-      return createDocumentResponse(
-          responseJsonObject.toString(),
-          new HashMap<String, Object>() {});
-
-    } catch (ModuleException me) {
-      throw me;
-
-    } catch (Exception e) {
-      throw new ModuleException(
-          String.format("Error while chunking."),
           MuleVectorsErrorType.DOCUMENT_OPERATIONS_FAILURE,
           e);
     }
@@ -175,12 +106,12 @@ public class DocumentOperations {
    * @return Result containing the chunked text segments and response attributes.
    */
   @MediaType(value = APPLICATION_JSON, strict = false)
-  @Alias("Text-chunk")
-  @DisplayName("[Text] Chunk")
+  @Alias("Document-chunk")
+  @DisplayName("[Document] Chunk")
   @Throws(DocumentErrorTypeProvider.class)
-  @OutputJsonType(schema = "api/metadata/TextListResponse.json")
+  @OutputJsonType(schema = "api/metadata/DocumentChunkResponse.json")
   public org.mule.runtime.extension.api.runtime.operation.Result<InputStream, DocumentResponseAttributes>
-  chunkText(@Config DocumentConfiguration documentConfiguration,
+  chunkDocument(@Config DocumentConfiguration documentConfiguration,
         @Alias("text") @DisplayName("Text") @Content String text,
         @ParameterGroup(name = "Segmentation") SegmentationParameters segmentationParameters) {
 
@@ -190,10 +121,15 @@ public class DocumentOperations {
                                                      segmentationParameters.getMaxSegmentSizeInChars(),
                                                      segmentationParameters.getMaxOverlapSizeInChars());
 
-      JSONObject responseJsonObject = null;
+
+      JSONArray responseJsonArray = new JSONArray();
+
+      for(TextSegment textSegment : textSegments){
+        responseJsonArray.put(textSegment.text());
+      }
 
       return createDocumentResponse(
-          responseJsonObject.toString(),
+          responseJsonArray.toString(),
           new HashMap<String, Object>() {});
 
     } catch (ModuleException me) {
--- src/main/java/org/mule/extension/vectors/internal/operation/EmbeddingOperations.java
+++ src/main/java/org/mule/extension/vectors/internal/operation/EmbeddingOperations.java
@@ -1,6 +1,5 @@
 package org.mule.extension.vectors.internal.operation;
 
-import static org.mule.extension.vectors.internal.constant.Constants.JSON_KEY_BASE64DATA;
 import static org.mule.extension.vectors.internal.constant.Constants.MEDIA_TYPE_IMAGE;
 import static org.mule.extension.vectors.internal.helper.ResponseHelper.createEmbeddingResponse;
 import static org.mule.extension.vectors.internal.helper.ResponseHelper.createMultimodalEmbeddingResponse;
@@ -192,114 +191,13 @@ public class EmbeddingOperations {
     }
   }
 
-  /**
-   * Generates embeddings from text segments provided in a document format.
-   *
-   * @param embeddingConfiguration the configuration for the embedding service.
-   * @param modelConnection the connection to the embedding model.
-   * @param content the input text segments as an {@link InputStream} in JSON format.
-   * @param embeddingModelParameters parameters for the embedding model to be used.
-   * @return a {@link org.mule.runtime.extension.api.runtime.operation.Result} containing the embeddings in JSON format and metadata.
-   * @throws ModuleException if an error occurs during the embedding process.
-   */
   @MediaType(value = APPLICATION_JSON, strict = false)
-  @Alias("Embedding-generate-from-document")
-  @DisplayName("[Embedding] Generate from document")
-  @Throws(EmbeddingErrorTypeProvider.class)
-  @OutputJsonType(schema = "api/metadata/EmbeddingGenerateResponse.json")
-  public org.mule.runtime.extension.api.runtime.operation.Result<InputStream, EmbeddingResponseAttributes>
-  generateEmbeddingFromDocument(@Config EmbeddingConfiguration embeddingConfiguration,
-                                @Connection BaseModelConnection modelConnection,
-                                @Alias("textSegments") @DisplayName("Text Segments") @InputJsonType(schema = "api/metadata/DocumentResponse.json") @Content InputStream content,
-                                @ParameterGroup(name = "Embedding Model") EmbeddingModelParameters embeddingModelParameters) {
-
-    try {
-
-      String contentString = IOUtils.toString(content, StandardCharsets.UTF_8);
-
-      BaseModel baseModel = BaseModel.builder()
-          .configuration(embeddingConfiguration)
-          .connection(modelConnection)
-          .embeddingModelParameters(embeddingModelParameters)
-          .build();
-
-      EmbeddingModel embeddingModel = baseModel.buildEmbeddingModel();
-
-      List<TextSegment> textSegments = new LinkedList<>();
-      List<Embedding> embeddings;
-      TokenUsage tokenUsage;
-
-      JSONObject jsonObject = new JSONObject(contentString);
-      JSONArray jsonTextSegments = jsonObject.getJSONArray(Constants.JSON_KEY_TEXT_SEGMENTS);
-
-      IntStream.range(0, jsonTextSegments.length())
-          .mapToObj(jsonTextSegments::getJSONObject) // Convert index to JSONObject
-          .forEach(jsonTextSegment -> {
-            Metadata metadata = Metadata.from(jsonTextSegment.getJSONObject(Constants.JSON_KEY_METADATA).toMap());
-            textSegments.add(new TextSegment(jsonTextSegment.getString(Constants.JSON_KEY_TEXT), metadata));
-          });
-
-      try {
-
-        Response<List<Embedding>> response = embeddingModel.embedAll(textSegments);
-        embeddings = response.content();
-        tokenUsage = response.tokenUsage() != null ?
-            new TokenUsage(response.tokenUsage().inputTokenCount() != null ? response.tokenUsage().inputTokenCount() : 0,
-                           response.tokenUsage().outputTokenCount() != null ? response.tokenUsage().outputTokenCount() : 0,
-                           response.tokenUsage().totalTokenCount() != null ? response.tokenUsage().totalTokenCount(): 0)
-            : null;
-
-      } catch(ModuleException e) {
-
-        throw e;
-
-      } catch(Exception e) {
-
-        throw new ModuleException(
-            String.format("Error while generating embedding from document \"%s\"", ""),
-            MuleVectorsErrorType.AI_SERVICES_FAILURE,
-            e);
-      }
-
-      JSONArray jsonEmbeddings = IntStream.range(0, embeddings.size())
-          .mapToObj(i -> {
-            return embeddings.get(i).vector();
-          })
-          .collect(JSONArray::new, JSONArray::put, JSONArray::putAll);
-
-      jsonObject.put(Constants.JSON_KEY_EMBEDDINGS, jsonEmbeddings);
-
-      jsonObject.put(Constants.JSON_KEY_DIMENSION, embeddingModel.dimension());
-
-      HashMap<String, Object> attributes = new HashMap<String, Object>() {{
-        put("embeddingModelName", embeddingModelParameters.getEmbeddingModelName());
-        put("embeddingModelDimension", embeddingModel.dimension());
-      }};
-      if(tokenUsage != null) {
-
-        attributes.put("tokenUsage", tokenUsage);
-      }
-      return createEmbeddingResponse(jsonObject.toString(), attributes);
-
-    } catch (ModuleException me) {
-      throw me;
-
-    } catch (Exception e) {
-
-      throw new ModuleException(
-          String.format("Error while generating embedding from document \"%s\"", ""),
-          MuleVectorsErrorType.EMBEDDING_OPERATIONS_FAILURE,
-          e);
-    }
-  }
-
-  @MediaType(value = APPLICATION_JSON, strict = false)
-  @Alias("Embedding-generate-from-binary")
-  @DisplayName("[Embedding] Generate from binary")
+  @Alias("Embedding-generate-from-media")
+  @DisplayName("[Embedding] Generate from media")
   @Throws(EmbeddingErrorTypeProvider.class)
   @OutputJsonType(schema = "api/metadata/EmbeddingGenerateResponse.json")
   public org.mule.runtime.extension.api.runtime.operation.Result<InputStream, MultimodalEmbeddingResponseAttributes>
-  generateEmbeddingFromBinary(@Config EmbeddingConfiguration embeddingConfiguration,
+  generateEmbeddingFromMedia(@Config EmbeddingConfiguration embeddingConfiguration,
                                     @Connection BaseModelConnection modelConnection,
                                     @ParameterGroup(name = "Media") MediaBinaryParameters mediaBinaryParameters,
                                     @ParameterGroup(name = "Embedding Model") EmbeddingModelParameters embeddingModelParameters) {
@@ -400,102 +298,6 @@ public class EmbeddingOperations {
     } catch (Exception e) {
       throw new ModuleException(
           "Error while generating embedding from media binary",
-          MuleVectorsErrorType.EMBEDDING_OPERATIONS_FAILURE,
-          e);
-    }
-  }
-
-  @MediaType(value = APPLICATION_JSON, strict = false)
-  @Alias("Embedding-generate-from-media")
-  @DisplayName("[Embedding] Generate from media")
-  @Throws(EmbeddingErrorTypeProvider.class)
-  @OutputJsonType(schema = "api/metadata/EmbeddingGenerateResponse.json")
-  public org.mule.runtime.extension.api.runtime.operation.Result<InputStream, MultimodalEmbeddingResponseAttributes>
-  generateEmbeddingFromMedia(@Config EmbeddingConfiguration embeddingConfiguration,
-                                    @Connection BaseModelConnection modelConnection,
-                                    @Alias("media") @DisplayName("Media") @InputJsonType(schema = "api/metadata/MediaLoadSingleResponse.json") @Content InputStream mediaContent,
-                                    @Alias("label") @DisplayName("Media Label") @Summary("Short text describing the image") @Example("An image of a sunset") @Content String label,
-                                    @ParameterGroup(name = "Embedding Model") EmbeddingModelParameters embeddingModelParameters) {
-
-    try {
-
-      String mediaContentString = IOUtils.toString(mediaContent, StandardCharsets.UTF_8);
-      JSONObject jsonMediaObject = new JSONObject(mediaContentString);
-
-      JSONObject jsonObject = new JSONObject();
-
-      List<TextSegment> textSegments = new LinkedList<>();
-      textSegments.add(TextSegment.from(label));
-      JSONArray jsonTextSegments = IntStream.range(0, textSegments.size())
-          .mapToObj(i -> {
-            JSONObject jsonSegment = new JSONObject();
-            jsonSegment.put(Constants.JSON_KEY_TEXT, textSegments.get(i).text());
-            JSONObject jsonMetadata = jsonMediaObject.getJSONObject(Constants.JSON_KEY_METADATA);
-            jsonMetadata.put(Constants.JSON_KEY_INDEX, i);
-            jsonSegment.put(Constants.JSON_KEY_METADATA, jsonMetadata);
-            return jsonSegment;
-          })
-          .collect(JSONArray::new, JSONArray::put, JSONArray::putAll);
-
-      jsonObject.put(Constants.JSON_KEY_TEXT_SEGMENTS, jsonTextSegments);
-
-      BaseModel baseModel = BaseModel.builder()
-          .configuration(embeddingConfiguration)
-          .connection(modelConnection)
-          .embeddingModelParameters(embeddingModelParameters)
-          .build();
-
-      // Assuming you have a multimodal embedding model method
-      EmbeddingMultimodalModel multimodalEmbeddingModel = (EmbeddingMultimodalModel) baseModel.buildEmbeddingMultimodalModel();
-
-      Embedding embedding;
-      TokenUsage tokenUsage;
-      String mediaType = jsonMediaObject.getJSONObject(Constants.JSON_KEY_METADATA).getString(Constants.METADATA_KEY_MEDIA_TYPE);
-      if (mediaType.equals(MEDIA_TYPE_IMAGE)) {
-
-        Response<Embedding> response = label != null && !label.isEmpty()
-            ?
-            multimodalEmbeddingModel.embedTextAndImage(label,
-                                                       Base64.getDecoder().decode(jsonMediaObject.getString(JSON_KEY_BASE64DATA)))
-            :
-                multimodalEmbeddingModel.embedImage(Base64.getDecoder().decode(jsonMediaObject.getString(JSON_KEY_BASE64DATA)));
-        embedding = response.content();
-        tokenUsage = response.tokenUsage() != null ?
-            new TokenUsage(response.tokenUsage().inputTokenCount() != null ? response.tokenUsage().inputTokenCount() : 0,
-                           response.tokenUsage().outputTokenCount() != null ? response.tokenUsage().outputTokenCount() : 0,
-                           response.tokenUsage().totalTokenCount() != null ? response.tokenUsage().totalTokenCount(): 0)
-            : null;
-      } else {
-        throw new ModuleException(
-            String.format("Media type %s not supported.", mediaType),
-            MuleVectorsErrorType.EMBEDDING_OPERATIONS_FAILURE);
-      }
-
-      JSONArray jsonEmbeddings = new JSONArray();
-      jsonEmbeddings.put(embedding.vector());
-
-      jsonObject.put(Constants.JSON_KEY_EMBEDDINGS, jsonEmbeddings);
-
-      jsonObject.put(Constants.JSON_KEY_DIMENSION, multimodalEmbeddingModel.dimension());
-
-
-      HashMap<String, Object> attributes = new HashMap<String, Object>() {{
-        put("embeddingModelName", embeddingModelParameters.getEmbeddingModelName());
-        put("embeddingModelDimension", multimodalEmbeddingModel.dimension());
-      }};
-      if (tokenUsage != null) {
-
-        attributes.put("tokenUsage", tokenUsage);
-      }
-      return createMultimodalEmbeddingResponse(jsonObject.toString(), attributes);
-
-    } catch(ModuleException me) {
-
-      throw me;
-
-    } catch (Exception e) {
-      throw new ModuleException(
-          "Error while generating embedding from image and text",
           MuleVectorsErrorType.EMBEDDING_OPERATIONS_FAILURE,
           e);
     }
