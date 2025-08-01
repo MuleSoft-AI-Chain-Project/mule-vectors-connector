522107bc24ce01e73daf749947222f8dd26679ca
lvarri <lvarri@salesforce.com>
2025-08-01 15:11:28 +0530

    munit addition for pg vector in fips mode
    (cherry picked from commit 4085e210de4e292e44287211c13b44efa90e9e68)

--- src/main/java/org/mule/extension/vectors/internal/error/exception/VectorsRuntimeException.java
+++ src/main/java/org/mule/extension/vectors/internal/error/exception/VectorsRuntimeException.java
@@ -3,17 +3,17 @@ package org.mule.extension.vectors.internal.error.exception;
 import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.api.i18n.I18nMessage;
 
-public class VectorsRuntimeException extends MuleRuntimeException{
+public class VectorsRuntimeException extends MuleRuntimeException {
 
-    public VectorsRuntimeException(I18nMessage message) {
-       super(message);
-    }
+  public VectorsRuntimeException(I18nMessage message) {
+    super(message);
+  }
 
-    public VectorsRuntimeException(I18nMessage message, Throwable cause) {
-       super(message, cause);
-    }
+  public VectorsRuntimeException(I18nMessage message, Throwable cause) {
+    super(message, cause);
+  }
 
-    public VectorsRuntimeException(Throwable cause) {
-        super(cause);
-    }
+  public VectorsRuntimeException(Throwable cause) {
+    super(cause);
+  }
 }
