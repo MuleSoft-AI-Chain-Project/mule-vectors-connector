a4cb48704d81a4891a2683c05b47ff52d37875cc
kislayforce <kislay.srivastava@salesforce.com>
2026-03-13 11:15:35 +0530

    @W-21334611 added unit tests to increase coverage

---
+++ src/test/java/org/mule/extension/vectors/internal/connection/storage/local/LocalStorageConnectionTest.java
@@ -0,0 +1,69 @@
+package org.mule.extension.vectors.internal.connection.storage.local;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.mule.extension.vectors.internal.constant.Constants;
+
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+class LocalStorageConnectionTest {
+
+  @TempDir
+  Path tempDir;
+
+  @Test
+  void constructor_assignsWorkingDir() {
+    LocalStorageConnection conn = new LocalStorageConnection("/tmp");
+    assertThat(conn.getWorkingDir()).isEqualTo("/tmp");
+    assertThat(conn.getStorageType()).isEqualTo(Constants.STORAGE_TYPE_LOCAL);
+  }
+
+  @Test
+  void initialise_withValidDirectory() {
+    LocalStorageConnection conn = new LocalStorageConnection(tempDir.toString());
+    conn.initialise();
+    assertThat(conn.getWorkingDir()).isEqualTo(tempDir.toString());
+  }
+
+  @Test
+  void initialise_withNullDir_defaultsToHome() {
+    LocalStorageConnection conn = new LocalStorageConnection(null);
+    conn.initialise();
+    assertThat(conn.getWorkingDir()).isNotNull();
+  }
+
+  @Test
+  void initialise_withNonExistentDir_throwsException() {
+    LocalStorageConnection conn = new LocalStorageConnection("/nonexistent/path/xyz123");
+    assertThatThrownBy(conn::initialise).isInstanceOf(RuntimeException.class);
+  }
+
+  @Test
+  void initialise_withFilePath_throwsException() throws Exception {
+    Path file = tempDir.resolve("not-a-dir.txt");
+    java.nio.file.Files.writeString(file, "content");
+    LocalStorageConnection conn = new LocalStorageConnection(file.toString());
+    assertThatThrownBy(conn::initialise).isInstanceOf(RuntimeException.class);
+  }
+
+  @Test
+  void validate_withValidDir_doesNotThrow() {
+    LocalStorageConnection conn = new LocalStorageConnection(tempDir.toString());
+    conn.validate();
+  }
+
+  @Test
+  void validate_withNullDir_doesNotThrow() {
+    LocalStorageConnection conn = new LocalStorageConnection(null);
+    conn.validate();
+  }
+
+  @Test
+  void disconnect_isNoOp() {
+    LocalStorageConnection conn = new LocalStorageConnection("/tmp");
+    conn.disconnect();
+  }
+}
