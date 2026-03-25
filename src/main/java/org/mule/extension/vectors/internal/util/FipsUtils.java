55aa3290c454dbce4f3fabb9e069ecdce9e5cdd5
asish-sf <asishkumar.sharma@salesforce.com>
2026-03-25 17:08:43 +0530

    added check fips140-3

--- src/main/java/org/mule/extension/vectors/internal/util/FipsUtils.java
+++ src/main/java/org/mule/extension/vectors/internal/util/FipsUtils.java
@@ -11,10 +11,12 @@ public class FipsUtils {
    * Set of constants pertaining to the FIPS
    *
   */
-  public static final String FIPS_SECURITY_MODEL = "fips140-2";
+  public static final String FIPS_140_2_SECURITY_MODEL = "fips140-2";
+  public static final String FIPS_140_3_SECURITY_MODEL = "fips140-3";
   public static final String PROPERTY_SECURITY_MODEL = "mule.security.model";
 
   public static boolean isFipsEnabled() {
-    return FIPS_SECURITY_MODEL.equals(getProperty(PROPERTY_SECURITY_MODEL));
+    return FIPS_140_2_SECURITY_MODEL.equals(getProperty(PROPERTY_SECURITY_MODEL))
+        || FIPS_140_3_SECURITY_MODEL.equals(getProperty(PROPERTY_SECURITY_MODEL));
   }
 }
